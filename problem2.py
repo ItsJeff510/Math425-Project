d573ee73b9f8c34fb4a5ca1cbbb85f9e23d09bf1
ItsJeff510 <jovannyr72@gmail.com>
2024-04-28 19:26:24 -0700

    Problem 2 Part A completed

--- problem2.py
+++ problem2.py
@@ -12,26 +12,35 @@ A, b = read_training_data("train.data")
 import numpy as np
 from sympy import Matrix
 
-#print(A.row)
-#print(len(A))
-
-A = np.array(A).astype(float)
+# Making the Array a 2-Dimensional Matrix
+A = np.array(A).astype(float) 
 b = np.array(b).astype(float)
 
-print("Before: ") 
-print(A[0][1])
+# Applying the weights to dataset #2, 8, 26, 27, 30
 for i in range(len(A)):
     for j in range(len(A[i])):
-        if j == 2:
-            A[i][j] /= 2
+        if j == 1:
+            A[i][1] /= 2
+        
+        elif j == 7:
+            A[i][7] /= 2
 
-print("After: ") 
-print(A[0][1])
-    
+        elif j == 25:
+            A[i][25] /= 2
+        
+        elif j == 26:
+            A[i][26] /= 2
 
+        elif j == 29:
+            A[i][29] /= 2
+
+# Getting the Q and R Matrix
 Q, R = np.linalg.qr(A)
 
-x_ls = np.linalg.solve(R, np.dot(Q.T, b))
 
-print("Solution to least squares problem:", x_ls)
+# Applying Least Squares
+least_squares = np.linalg.solve(R, np.dot(Q.T, b))
+
+# Displaying the results
+print("Solution to least squares problem:\n", least_squares)
 

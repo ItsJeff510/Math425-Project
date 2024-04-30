f17973594131d8c36c6b8cb3b7724a6ec1ec31d0
Edwin Wu <ewu8@sfsu.edu>
2024-04-29 19:27:44 -0700

    Reworked problem 1a) to use QR algorithm instead of RREF, Created classifier function to predict malignancy of validate.data for problem 1b)

--- problem1.py
+++ problem1.py
@@ -1,36 +1,72 @@
 from efficient_cancer_data import read_training_data
 
 A, b = read_training_data("train.data")
-#print(A)
-A_t = A.transpose()
-lhs = A_t*A
-rhs = A_t*b
-concat = (lhs.row_join(rhs))
-concat = concat.rref()
+
+# Problem 1a) Use the QR algorithm to find the least-squares linear model for the data.
+
+# QR Factorization is just rewriting A = QR,
+# where Q is an orthonormal basis for Col(A)
+# and R is a triangular invertible matrix with positive diagonal entries.
+
+# Find Q and R
+Q, R = A.QRdecomposition()
+
+# The least-squares solution with QR factorization is denoted by x_hat = R^-1 * Q_t * b
+R_inv = R.inv()
+Q_t = Q.T
+least_squares = R_inv * Q_t * b # Could also have used A.QRSolve(b)
+
+# print(least_squares)
+
+# Problem 1b) Use the linear model from (a) to the data set validate.data
+# and predict the malignancy of the tissues. You will have to define a
+# classifier function C(y) = 1 if prediction is non-negative, -1 otherwise
+
+validate_data, actual_malignancy = read_training_data("validate.data")
+predicted_malignancy = validate_data * least_squares
+# print(len(predicted_malignancy))
+
+# Replaces elements in vector y with +-1 in place
+def classifier(y):
+    for i in range(len(y)):
+        if y[i] >= 0:
+            y[i] = 1
+        else:
+            y[i] = -1
+    
+    return y
+
+predicted_malignancy = classifier(predicted_malignancy)
+print(predicted_malignancy)
+
+# Problem 1c) What is the percentage of samples that are incorrectly classified?
+# Is it greater or smaller than the success rate on the training data?
+
+
+# Setting up the least squares solution (A_t * A * x_hat = A_t * b)
+# A_t = A.transpose()
+# lhs = A_t*A
+# rhs = A_t*b
+# concat = (lhs.row_join(rhs))
+# concat = concat.rref()
 #print(concat)
 
-coefficients_list = [] #will contain the coefficients for our linear model
+# coefficients_list = [] #will contain the coefficients for our linear model
 
 # output the equation for the linear model
-print("Linear model: y = ", end="")
-for i in range(len(concat[1])):
-    print((concat[0])[i,len(concat[1])], end="")
-    coefficients_list.append((concat[0])[i,len(concat[1])])
-    print("x", end="")
-    print(i, end="")
-    if i != (len(concat[1]) - 1):
-        print(" + ", end="")
+# print("Linear model: y = ", end="")
+# for i in range(len(concat[1])):
+#     print((concat[0])[i,len(concat[1])], end="")
+#     coefficients_list.append((concat[0])[i,len(concat[1])])
+#     print("x", end="")
+#     print(i, end="")
+#     if i != (len(concat[1]) - 1):
+#         print(" + ", end="")
 
-print("\n\nList of coefficients:\n")
-print(coefficients_list)
+# print("\n\nList of coefficients:\n")
+# print(coefficients_list)
+# print()
 
-
-
-
-
-#x = A.QRsolve(b)
-
-#print(x)
 
 # for i in range(A.shape[0]):
 #     for j in range(A.shape[1]):
